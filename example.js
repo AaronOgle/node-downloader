e7b47656cbbe230b5bad53bdce312d202893925c
Aaron Ogle <aaron@geekgonecrazy.com>
2017-04-25 09:46:45 -0500

    Update example to handle error

--- example.js
+++ example.js
@@ -10,12 +10,16 @@ http.createServer(function (req, res) {
  	} else {
  		var download = new Downloader.CurlDownloader();
 
-		download.setDirToSave('~/Desktop/abc/123/456/789/qrst/');
+		download.setDirToSave('~/');
 		download.downloadFile('http://127.0.0.1:8124/http://ipv4.download.thinkbroadband.com/10MB.zip', '10MB.zip');
 		// Alternatively download.downloadFile(req.url.substr(1), 'filename.zip');
 		
 		download.eventEmitter.on('progress', function(percent, speed) {
 			console.log('Percent: ' + parseFloat(percent));
+		});
+
+		download.eventEmitter.on('error', function(err) {
+			console.log('Error Occured:', err);
 		});
 
 		download.eventEmitter.on('end', function(code) {
